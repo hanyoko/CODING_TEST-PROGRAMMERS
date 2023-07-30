b168212dd4078be24323278164cac8d47efab808
yohan <koyohanyoko@gmail.com>
2023-07-30 23:34:46 +0900

    DAY 14

--- DAY 14/문자열을 정수로 바꾸기.js
+++ DAY 14/문자열을 정수로 바꾸기.js
@@ -8,6 +8,9 @@ function solution(s) {
 }
 
 //다른 사람의 풀이
+function solution(s){
+    return s/1;
+    }
 
-
-
+//사칙연산 되면서 문자가 자동으로 파싱이 되는 것 ....
+//새로운 세계 .... 새롭다
--- DAY 14/정수 제곱근 판별.js
+++ DAY 14/정수 제곱근 판별.js
@@ -1,9 +1,24 @@
 //나의 풀이
-
+function solution(n) {
+    return Math.sqrt(n) % 1 == 0 ? Math.pow(Math.sqrt(n) + 1, 2) : -1;
+}
 
 
 
 //다른 사람의 풀이
+function solution(n) {
+    var answer = 0;
 
+    for(let i=0, a=0; i<=n; i++){
+      if(n/i===i){
+        a = i + 1
+        return a*a
+      } else {
+        answer = -1
+      }
+    }
+
+    return answer;
+}
 
 
