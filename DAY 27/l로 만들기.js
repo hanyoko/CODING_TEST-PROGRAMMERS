96a6c6f8ba14b483574842f0fc7d22933f4d26c6
yohan <koyohanyoko@gmail.com>
2023-08-15 18:42:20 +0900

    DAY 27

--- DAY 27/l로 만들기.js
+++ DAY 27/l로 만들기.js
@@ -1,9 +1,13 @@
 //나의 풀이
+function solution(myString) {
+    return myString.replace(/[a-k]/g,'l')
+}
 
-
-
+//정규표현식을 이용해서 a부터 k까지는 l로 변환시켰다.
 
 //다른 사람의 풀이
+function solution(myString) {
+    return [...myString].map((v) => v < 'l' ? 'l' : v).join('');
+}
 
-
-
+//문자 자체로도 비교가 되구나 .... 
--- DAY 27/배열의 원소 삭제하기.js
+++ DAY 27/배열의 원소 삭제하기.js
@@ -1,9 +1,6 @@
 //나의 풀이
+function solution(arr, delete_list) {
+    return arr.filter(a => !delete_list.includes(a));
+}
 
-
-
-
-//다른 사람의 풀이
-
-
-
+//filter를 이용해서 delete_list에 포함되지 않는 것을 리턴했다
