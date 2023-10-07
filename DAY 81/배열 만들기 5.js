0a9ddd6cf27e9a693b28894d9f1fc0608b1b3a38
yohan <koyohanyoko@gmail.com>
2023-10-07 18:44:02 +0900

    DAY 81

--- DAY 81/배열 만들기 5.js
+++ DAY 81/배열 만들기 5.js
@@ -6,7 +6,7 @@ function solution(intStrs, k, s, l) {
     for(let x of intStrs){
         //x원소를 split을 통해 배열로 만들어주고 splice로 s번째부터 l까지 추출하여 
         //join('')을 이용해 문자열로 다시 만들어 준것을 result 배열에 넣어준다. 
-        result =Number(x.split('').splice(s,l).join(''));
+        result = Number(x.split('').splice(s,l).join(''));
         // result에서 k보다 큰것을 answer에 넣어준다.
         if(result>k){
             answer.push(result);
