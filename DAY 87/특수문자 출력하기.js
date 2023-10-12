270bd474e541961ec4695bd97c737147fce3015b
yohan <koyohanyoko@gmail.com>
2023-10-13 00:38:01 +0900

    DAY 88

---
+++ DAY 87/특수문자 출력하기.js
@@ -0,0 +1,18 @@
+//나의 풀이
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('close', function () {
+    console.log(`!@#$%^&*(\\\'\"<>?:;`)
+});
+
+/*
+역슬래시 사용해서 특수문자 표현하는 법
+
+역슬래시 (＼) : ＼＼
+큰따옴표(") : ＼"
+작은따옴표(') : ＼'
+*/
