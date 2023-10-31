7bc82cc5823d279496617a4917563d8b8fe000e1
yohan <koyohanyoko@gmail.com>
2023-10-31 23:53:34 +0900

    DAY 102

--- DAY 102/이차원 배열 대각선 순회하기.js
+++ DAY 102/이차원 배열 대각선 순회하기.js
@@ -1,9 +1,23 @@
 //나의 풀이
-
+function solution(board, k) {
+    let answer = 0;
+    for(let i = 0; i < board.length; i++) {
+        for(let j = 0; j < board[i].length; j++) {
+            if(i+j <= k) answer += board[i][j];
+        }
+    }
+    return answer;
+}
 
 
 
 //다른 사람의 풀이
+function solution(board, k) {
+    return board.reduce(
+      (total, row, i) => total + row.reduce((prev, num, j) => (i + j <= k ? prev + num : prev), 0),
+      0,
+    );
+  }
 
 
-
+//reduce 사용
